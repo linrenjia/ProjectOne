c883c4346148e9392aba0d26af1067673260c71e
linrj <linrj@zdvictory.com>
2020-03-15 22:15:09 +0800

    user1提交3

--- controller/src/main/java/ApiController.java
+++ controller/src/main/java/ApiController.java
@@ -4,6 +4,7 @@ public class ApiController {
 
     public void login(){
 
+        System.out.println("本地提交");
         System.out.println("服务器版本1");
         System.out.println("服务器版本1");
         System.out.println("服务器版本1");
