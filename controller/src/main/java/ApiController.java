073487158b9acca5712959dc0713f91a76938ee2
linrj <linrj@zdvictory.com>
2020-03-15 10:00:41 +0800

    本地提交pom

--- controller/src/main/java/ApiController.java
+++ controller/src/main/java/ApiController.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ApiController {
@@ -7,5 +9,7 @@ public class ApiController {
     public void login(){
         System.out.println();
         System.out.println("this is linrejia");
+        List<String> ss = new ArrayList<>();
+        Set<String> set = new HashSet<>();
     }
 }
