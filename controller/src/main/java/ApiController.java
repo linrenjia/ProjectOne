c1f9695c6aa64618b524271ad4b8884c8342c34b
linrj <linrj@zdvictory.com>
2020-03-15 11:15:30 +0800

    user1提交3

--- controller/src/main/java/ApiController.java
+++ controller/src/main/java/ApiController.java
@@ -12,10 +12,6 @@ public class ApiController {
 
         Integer aaa = 12;
 
-        /************/
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        boolean abc = true;
+
     }
 }
